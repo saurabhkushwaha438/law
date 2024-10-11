57dc1a0495251c7ee33ff752aaceafc91598e647
saurabh kushwaha <saurabhkushwah438@gmail.com>
2024-10-11 17:03:46 +0530

    change in backend after deploying frontend

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors({
-   origin: 'http://localhost:5173/',
+   origin: 'https://legaleyepartner.onrender.com',
 }));
 connectDB();
 
