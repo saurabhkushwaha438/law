e2f3728d9357773fda99453c7aab2dcb7af37eb6
saurabh kushwaha <saurabhkushwah438@gmail.com>
2024-10-11 22:05:28 +0530

    port in index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
